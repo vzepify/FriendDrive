cb2277a1b0db37d5a13d77badf1b40fb023ad818
NeoNarwhal <142155258+vzepify@users.noreply.github.com>
2026-08-31 21:22:32 -0700

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://douqaufjgftxsqdatuxh.supabase.co/rest/v1/";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_TaedZhqTJM590wmh0scEyw_iBLu9_Hq";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
