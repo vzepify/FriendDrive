9b3c631091761e7d6ca7fe599970fefec4d729ee
NeoNarwhal <142155258+vzepify@users.noreply.github.com>
2026-08-31 21:57:46 -0700

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://douqaufjgftxsqdatuxh.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://douqaufjgftxsqdatuxh.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_TaedZhqTJM590wmh0scEyw_iBLu9_Hq";
 
 const supabaseClient = supabase.createClient(
